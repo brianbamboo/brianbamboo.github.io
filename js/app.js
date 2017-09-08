de05dce171304e316368a8eb8285ba37143e118a
Brian Wang <brianwang92@gmail.com>
2017-09-07 18:29:10 -0700

    Added some more projects

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
 var app = angular.module('PortfolioApp', []).controller('MainCtrl', ['$scope', function($scope) {
 	$scope.projects = [
+		{ title : 'An Analysis of Public Wi-Fi in NYC',
+		  link  : 'projects/nyc-wifi.html',
+		  date  : 'Sep 2017',
+		  description: "I don't use Python for data analysis as much as I do in R, so I decided to perform this simple analysis on Wi-Fi hotspots in NYC using Jupyter Notebook. Data processing in pandas, visualizations with seaborn, and regression with statsmodels.",
+		  image : 'assets/proj8.png'
+		},
+
+		{ title : 'An Analysis of City Trees in SF',
+		  link  : 'projects/sf_trees.html',
+		  date  : 'Sep 2017',
+		  description: "It's been a while since I've mapped something, so it was pretty cool to create this density map of city trees in San Francisco, along with learning some information through some simple tabulations and graphs. Mapping done with ggmap.",
+		  image : 'assets/proj7.png'
+		},
+
+		{ title : 'An Analysis of Airplane Crashes since 1908',
+		  link  : 'projects/airplanes.html',
+		  date  : 'Aug 2017',
+		  description: "Picked up this dataset off kaggle, and created a bunch of visualizations. Learned that there were a lot of airplane crashes in WWII. The Germans crashed a lot less than the Americans. Also, don't fly Aeroflot.",
+		  image : 'assets/proj6.png'
+	    },
+
 		{ title : 'An Analysis of City of Chicago Employee Salaries',
-		  link  : 'salaries.html',
+		  link  : 'projects/salaries.html',
 		  date  : 'July 2017',
 		  description : "For this project, I analyzed a dataset on city of Chicago employee salaries, created some data visualizations and learned that there is a person in Chicago that earns an annual salary of $1. For real. It's a thing.",
 		  image : 'assets/proj5.png'
